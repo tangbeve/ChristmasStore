812e9860589686b52836bade53fe70ab3faf0057
tangbeve <tlbeverly96@gmail.com>
2019-12-17 08:30:44 -0500

    Added models folder for user

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CodingDojo.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int ID { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
